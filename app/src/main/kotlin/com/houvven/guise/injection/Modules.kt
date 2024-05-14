9720287d7495bf2f70fa1bb1d00d75db03d5ada6
houvven <houvven@gmail.com>
2024-05-14 10:08:24 +0800

    add ModuleStore.Hooker into modules

--- app/src/main/kotlin/com/houvven/guise/injection/Modules.kt
+++ app/src/main/kotlin/com/houvven/guise/injection/Modules.kt
@@ -2,7 +2,12 @@ package com.houvven.guise.injection
 
 import com.houvven.guise.BuildConfig
 import com.houvven.guise.data.AppsStore
+import com.houvven.guise.hook.store.ModuleStore
+import com.houvven.guise.hook.store.impl.MediaModuleStore
+import com.houvven.guise.hook.store.impl.SharedPreferenceModuleStore
 import com.houvven.guise.ui.screen.launch.home.HomeViewModel
+import com.houvven.guise.ui.screen.launch.person.PersonViewModel
+import com.houvven.guise.ui.screen.profile.AppProfileReviseViewModel
 import com.houvven.guise.util.app.AppScanner
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModelOf
@@ -24,12 +29,21 @@ val commonModule = module {
 val storeModule = module {
     // Provide the AppsStore
     single<AppsStore> {
-        AppsStore(
-            appScanner = get()
-        )
+        AppsStore(appScanner = get())
+    }
+    // ModuleStore
+    single<ModuleStore.Hooker> {
+        val isUsingNRootFramework = false
+        if (isUsingNRootFramework) {
+            MediaModuleStore.Hooker()
+        } else {
+            SharedPreferenceModuleStore.Hooker(androidContext())
+        }
     }
 }
 
 val viewModelModule = module {
     viewModelOf(::HomeViewModel)
+    viewModelOf(::AppProfileReviseViewModel)
+    viewModelOf(::PersonViewModel)
 }
