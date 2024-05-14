7e7b3bf59a5671b8d9b704f154f1276058545d6d
houvven <houvven@gmail.com>
2024-05-14 21:52:32 +0800

    ProfileSuggest add a creator

--- app/src/main/kotlin/com/houvven/guise/data/domain/ProfileSuggest.kt
+++ app/src/main/kotlin/com/houvven/guise/data/domain/ProfileSuggest.kt
@@ -1,6 +1,15 @@
 package com.houvven.guise.data.domain
 
-class ProfileSuggest<T> private constructor(
+class ProfileSuggest<T>(
     val label: String,
     val value: T
-)
+) {
+
+    companion object {
+        fun <T> create(vararg profiles: Pair<String, T>): List<ProfileSuggest<T>> {
+            return profiles.map {
+                ProfileSuggest(it.first, it.second)
+            }
+        }
+    }
+}
