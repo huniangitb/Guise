6ae1f7c69c2a685bf7a767cb7f3dc2d87109e202
houvven <houvven@gmail.com>
2024-05-20 17:24:17 +0800

    Support search on enum editor

--- app/src/main/kotlin/com/houvven/guise/ui/screen/profile/components/ProfileReviseEditorContent.kt
+++ app/src/main/kotlin/com/houvven/guise/ui/screen/profile/components/ProfileReviseEditorContent.kt
@@ -1,23 +1,26 @@
 package com.houvven.guise.ui.screen.profile.components
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Clear
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.RadioButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
@@ -30,6 +33,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
@@ -37,7 +41,9 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import com.houvven.guise.R
 import com.houvven.guise.data.domain.ProfileSuggest
+import com.houvven.guise.ui.style.OutlinedTextFieldTransparentBorderColor
 import org.koin.compose.koinInject
 
 @Composable
@@ -180,27 +186,48 @@ fun <T> ProfileReviseEditor.Enum<T>.EditorContent(
     val onClick: (ProfileSuggest<T>) -> Unit = {
         state.updateAndDone(profiles.onSelectedChange(it))
     }
-    LazyColumn {
-        items(items = options, key = { it.label + it.value }) {
+    var query by remember { mutableStateOf("") }
+
+    if (options.size > 10) {
+        OutlinedTextField(
+            value = query,
+            onValueChange = { query = it },
+            modifier = Modifier.fillMaxWidth(),
+            colors = OutlinedTextFieldTransparentBorderColor,
+            placeholder = { Text(text = stringResource(id = R.string.search_hint)) },
+            leadingIcon = {
+                Icon(Icons.Default.Search, contentDescription = null)
+            }
+        )
+        HorizontalDivider()
+    }
+
+    LazyColumn(
+        contentPadding = PaddingValues(vertical = 5.dp)
+    ) {
+        items(
+            items = options.filter { it.label.contains(query.trim()) },
+            contentType = { it.value },
+            key = { it.value.toString() }
+        ) {
             val selected = value == it.value
             val containerColor =
-                if (selected) MaterialTheme.colorScheme.primaryContainer
-                else Color.Transparent
+                if (selected) MaterialTheme.colorScheme.primaryContainer else Color.Transparent
 
-            Card(
-                onClick = { onClick(it) },
-                colors = CardDefaults.cardColors(containerColor = containerColor)
-            ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(8.dp)
-                ) {
-                    RadioButton(selected = selected, onClick = { onClick(it) })
-                    Text(text = it.label)
-                }
-            }
+            ListItem(
+                headlineContent = { Text(text = it.label) },
+                modifier = Modifier
+                    .clip(MaterialTheme.shapes.medium)
+                    .clickable { onClick(it) }
+                    .animateItem(),
+                trailingContent = {
+                    if (selected) Icon(
+                        Icons.Default.Check,
+                        contentDescription = null
+                    )
+                },
+                colors = ListItemDefaults.colors(containerColor = containerColor),
+            )
         }
     }
 }
