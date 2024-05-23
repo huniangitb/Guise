4c964e15f0b180582b7841adc273375cf03f1f7b
houvven <houvven@gmail.com>
2024-05-23 11:20:35 +0800

    fixup! LSPosed scope sync

--- hook/src/main/kotlin/com/houvven/guise/hook/ModuleStatus.kt
+++ hook/src/main/kotlin/com/houvven/guise/hook/ModuleStatus.kt
@@ -1,7 +1,11 @@
 package com.houvven.guise.hook
 
 import com.highcapable.yukihookapi.YukiHookAPI
+import com.highcapable.yukihookapi.hook.core.api.compat.type.ExecutorType
 
 object ModuleStatus {
-    val isModuleActive = YukiHookAPI.Status.isXposedModuleActive
+    val isModuleActive = YukiHookAPI.Status.isModuleActive
+    val executorType = YukiHookAPI.Status.Executor.type
+
+    val isLSPosedExecutor = executorType == ExecutorType.LSPOSED_LSPATCH
 }
